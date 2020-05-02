38311d80ec78f73027bff1a5abd4cc7f34b0f124
CodyBrewer <codybrewerdev@gmail.com>
2020-05-02 12:01:29 -0600

    tests written adn passwing

--- auth/auth-router.test.js
+++ auth/auth-router.test.js
@@ -8,13 +8,82 @@ beforeEach(async () => {
 });
 
 describe('POST /register', () => {
-  it.todo('should return a 400 when missing a username or password');
-  it.todo('should return a 201 when registering a user is successful');
-  it.todo('should successfully register a new user');
+  it('should return a 401 when missing a username or password', async () => {
+    const missingPassword = { username: 'stan' };
+    const missingUsername = { password: 'hello' };
+    const missingBoth = {};
+
+    const missingPassRes = await request(server)
+      .post('/api/auth/register')
+      .send(missingPassword);
+
+    const missingUserRes = await request(server)
+      .post('/api/auth/register')
+      .send(missingUsername);
+
+    const missingBothRes = await request(server)
+      .post('/api/auth/register')
+      .send(missingBoth);
+
+    expect(missingPassRes.status).toBe(401);
+    expect(missingUserRes.status).toBe(401);
+    expect(missingBothRes.status).toBe(401);
+  });
+  it('should return a 201 when registering a user is successful', async () => {
+    const user = { username: 'test', password: 'password123' };
+    const res = await request(server)
+      .post('/api/auth/register')
+      .send(user);
+
+    expect(res.status).toBe(201);
+  });
+  it('should successfully register a new user', async () => {
+    const user = { username: 'stan', password: 'smith' };
+    const res = await request(server)
+      .post('/api/auth/register')
+      .send(user);
+
+    const users = await db.select('*').from('users');
+    expect(users.length).toBe(1);
+    expect(users[0].username).toBe('stan');
+    expect(users[0].password).not.toBe('smith');
+  });
 });
 
 describe('POST /login', () => {
-  it.todo('should return a 200 when a user successfully logs in');
-  it.todo('should return a token with the user\'s id when a user successfully logs in')
-  it.todo('should return a 401 when the user provides invalide credenitals');
+  beforeEach(async () => {
+    await request(server)
+      .post('/api/auth/register')
+      .send({ username: 'stan', password: 'smith' });
+  });
+
+  it('should return a 200 when a user successfully logs in', async () => {
+    const user = { username: 'stan', password: 'smith' };
+    
+    const res = await request(server)
+      .post('/api/auth/login')
+      .send(user);
+    
+    expect(res.status).toBe(200);
+  });
+  it('should return a token with the user\'s id when a user successfully logs in', async () => {
+    const user = { username: 'stan', password: 'smith' };
+    
+    const res = await request(server)
+    .post('/api/auth/login')
+    .send(user);
+    
+    const decoded = jwt.decode(res.body.token);
+    
+    expect(decoded.sub).toBe(1);
+  });
+  it('should return a 401 when the user provides invalid credenitals', async () => {
+    const user = { username: 'stan', password: 'roger' };
+
+    const res = await request(server)
+    .post('/api/auth/login')
+    .send(user);
+
+    expect(res.status).toBe(401);
+  });
 })
